e0a38ff1141bfcb9b4269fd445b5549124172848
vkope <vkoplin@hotmail.com>
2020-07-11 10:56:34 -0600

    First Commit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 50f;
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            else 
+            {
+                audioSource.Stop();
+            }
+            
+        }
+    }
+    void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.A))
+        {
+           transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+           transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;
+      
+    }
+}
